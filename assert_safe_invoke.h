b661a4fe781956d9eea3af02c148e1e57369dfac
Shpegun60 <Shpegun60@gmail.com>
2024-05-05 08:21:52 -0700

    some rename

--- assert_safe_invoke.h
+++ assert_safe_invoke.h
@@ -1,8 +1,8 @@
 #ifndef ASSERT_SAFE_INVOKE_H
 #define ASSERT_SAFE_INVOKE_H
 
-#define AE_DEFAULT_FOO(foo) (__typeof(foo))null_func
-#define AE_FUNCTION_PTR(type, name) type name = ( (type)null_func )
+#define AE_SAFE_FOO(foo) ( (__typeof(foo))(null_func) )
+#define AE_SAFE_FUNC_PTR(type, name) type name = ( (type)(null_func) )
 
 // declaration of default function
 void null_func(void);
@@ -19,7 +19,7 @@ void null_func(void);
 #include <stdio.h>
 typedef double (* calc_foo)(int a, int b, int c, int d, int e, int i);
 int main() {
-    AE_FUNCTION_PTR(calc_foo, foo);
+    AE_SAFE_FUNC_PTR(calc_foo, foo);
 
     int sum = foo(2, 3, 4, 5, 6, 7); // not work, programm is exit with error 3
     printf("SUM= %d", sum);
